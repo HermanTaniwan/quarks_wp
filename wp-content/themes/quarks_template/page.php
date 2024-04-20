15586a367e6cfafdd283e8c9dd4c71408d30aeec
hermantaniwan <taniwanherman@yahoo.com>
2024-04-20 10:48:29 +0700

    add template page

--- wp-content/themes/quarks_template/page.php
+++ wp-content/themes/quarks_template/page.php
@@ -1,35 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="auto">
 
-<head>
-    <meta charset="UTF-8" />
-    <title><?php echo get_bloginfo('name') ?></title>
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <meta name="description" content="Alat untuk mempermudah analisa seorang investor fundamental" />
-    <link href="https://cdn.jsdelivr.net/npm/modern-normalize@v2.0.0/modern-normalize.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css" rel="stylesheet" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= get_template_directory_uri() ?>/assets/img/favicons/atom.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= get_template_directory_uri() ?>/assets/img/favicons/atom.png">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Signika:wght@300..700&display=swap" rel="stylesheet">
-
-    <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="apple-touch-icon.png">
-    <meta property="og:title" content=Fundamental Investor Tools>
-    <meta property="og:site_name" content="Quarks.id">
-    <meta property="og:url" content="<?= site_url() ?>">
-    <meta property="og:description" content="Alat untuk mempermudah analisa seorang investor fundamental">
-    <meta property="og:type" content="website">
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:title" content="quarks">
-    <meta name="twitter:description" content="Alat untuk mempermudah analisa seorang investor fundamental">
-
-    <link href="<?= get_template_directory_uri() ?>/assets/style/dashboard.css" rel="stylesheet">
-    <link href="https://unpkg.com/@jarstone/dselect/dist/css/dselect.css" rel="stylesheet">
-
-    <meta name="theme-color" content="#712cf9">
+<?php get_header(); ?>
 
 <body data-bs-theme="dark">
 
@@ -70,8 +42,27 @@
         </div>
     </nav>
 
-    <article class="content px-3 py-5 p-md-5">
+    <div class="inline-block" style="height: 80px;"></div>
 
-        <?php the_content(); ?>
-    </article>
+
+
+    <div class="container-fluid">
+        <div class="wrapper row">
+            <main class=" workspace resource-area">
+
+                <div class="container mt-3">
+
+                    <div class="row">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+
+
+            </main>
+
+            <div class="col-lg-2"></div>
+        </div>
+    </div>
+
+
     <?php get_footer(); ?>
